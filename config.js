9fc42e9dba9f07e5d59505a432f48e69e37ad41c
Kaveemd17 <slkaveemd@gmail.com>
2025-03-29 13:32:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "bIJH0RxK#yBnLBP9ZFRWTy1Y4lQnI5MsBfew6Ruex_GYTbnNjNBg",
+  SESSION_ID: process.env.SESSION_ID || "HEh2QAgb#uheR_6eDhdDdQlieNIqcogeBwGM0BbAfzAP0nl1IJIo",
   MONGODB: process.env.MONGODB || "mongodb://mongo:tHzGJWpKgjtWjOSWpeWXOTozZBiRWtsn@maglev.proxy.rlwy.net:27598",
   OWNER_NUM: process.env.OWNER_NUM || "94741839074",
 };
