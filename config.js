23c0b0d4d2cee4e17a8daba6fa37eec69bd35fcd
Kaveemd17 <slkaveemd@gmail.com>
2025-03-30 07:37:14 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,9 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "HEh2QAgb#uheR_6eDhdDdQlieNIqcogeBwGM0BbAfzAP0nl1IJIo",
   OWNER_NUM: process.env.OWNER_NUM || "94741839074",
-  PREFIX: process.env.PREFIX || ".", 
+  PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Kaveemd17/BOT-IMGS/refs/heads/main/4e10a675-5de7-4552-8c31-b5dc1a31ed01.webp",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n*👻MADE BY KAVEE-MD👻*",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE: process.env.MODE || "private",
 };
